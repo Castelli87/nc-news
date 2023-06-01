397096a5a48a44dfd73a17109213985c32d659e3
Castelli87 <davidecastelli87@hotmail.com>
2023-06-01 14:17:42 +0100

    imported the commentList component becasue was breaking the app

--- src/Components/ArticleMainCard.jsx
+++ src/Components/ArticleMainCard.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { fetchArticle } from "../Api/api";
+import CommentsList from "./CommentsList";
 
 
 function ArticleMainCard() {
